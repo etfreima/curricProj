655e6d2dc16649d05fe5b44b0b35827b12bfafa6
etfreima <etfreima2080@gmail.com>
2018-04-02 11:05:40 -0400

    Update and rename index.java to curricProj.java
    Added functionality to main method. This acts as a main menu/welcome menu.

---
+++ curricProj.java
@@ -0,0 +1,23 @@
+package curricproj;
+import java.util.Scanner;
+/**
+ *
+ * @author efreima
+ */
+public class CurricProj {
+
+    
+    public static void main(String[] args) {
+        //Declare variables
+        Scanner userInput = new Scanner(System.in);
+        
+        println("Curriculum Viewer v0.1\n1. Create Schedule\n2. Create current progress\n3. View required classes");
+        String uI = userInput.next();
+    }
+    
+    
+    public static void println(String args) {
+        System.out.println(args);
+}
+}
+
