484cc251bace93c2e0fbbfa3673cfda6877f477c
etfreima <etfreima2080@gmail.com>
2018-04-02 11:08:30 -0400

    Update curricProj.java

--- curricProj.java
+++ curricProj.java
@@ -1,3 +1,4 @@
+
 package curricproj;
 import java.util.Scanner;
 /**
@@ -10,9 +11,10 @@ public class CurricProj {
     public static void main(String[] args) {
         //Declare variables
         Scanner userInput = new Scanner(System.in);
-        
-        println("Curriculum Viewer v0.1\n1. Create Schedule\n2. Create current progress\n3. View required classes");
+        println("Curriculum Viewer v0.1\n1. Create current progress\n2.Exit");
         String uI = userInput.next();
+        println("Curriculum Viewer v0.1\n1. Create Schedule\n2. Create current progress\n3. View required classes");
+        String uI2 = userInput.next();
     }
     
     
