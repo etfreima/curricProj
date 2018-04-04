0c11d570c837c614675986b7c3288aa82ab8e9ea
etfreima <etfreima2080@gmail.com>
2018-04-04 11:33:42 -0400

    Update classCategories.java

--- classCategories.java
+++ classCategories.java
@@ -1,1 +1,19 @@
 //array for classes go here
+
+package curricproj;
+
+public class classCategories {
+
+    public static String[][] classArray = new String[3][3];
+    
+    public static void classMatrix() {
+        
+        classArray[0] = new String[3]; //CET classes
+        classArray[1] = new String[3]; //other category
+        classArray[2] = new String[3]; //other category
+
+
+    }
+    //public static int = 
+    
+}
