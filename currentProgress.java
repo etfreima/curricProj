7e8e49c2fc9d7c63643bea7cd4d437e917b0e45c
etfreima <etfreima2080@gmail.com>
2018-04-04 11:34:28 -0400

    Update currentProgress.java

--- currentProgress.java
+++ currentProgress.java
@@ -3,4 +3,9 @@ package curricproj;
 
 public class currentProgress {
     public static void view (classCategories.classMatrix);
+    for(int i = 0; i < classCategories.classArray.length; i++){
+        for(int j = 0; j < numRows;j++){
+            
+        }
+    }
 }
